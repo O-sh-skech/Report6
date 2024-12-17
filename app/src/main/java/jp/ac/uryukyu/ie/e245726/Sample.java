df4bdb60ae5bb83f889a1b9cf00b09ba193c63c8
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2024-12-17 18:16:11 +0900

    調査3

--- app/src/main/java/jp/ac/uryukyu/ie/e245726/Sample.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245726/Sample.java
@@ -1,43 +1,54 @@
 package jp.ac.uryukyu.ie.e245726;
 
-import java.util.stream.IntStream;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Sample extends Application{
+
+    Stage window;
+    Scene scene1, scene2;
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    Button button;
-    Button button2;
-    String[] palamate = {"x","y","z"};
 
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the Window");
+        window = primaryStage;
 
-        StackPane layout = new StackPane();//レイアウト
+        Label label1 = new Label("welcom to first scene");
+        Button button1 = new Button("go to scene2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        IntStream.range(0, palamate.length).forEach(i -> {
-            Button button = new Button();
-            button.setText(palamate[i]);
-            button.setOnAction(e -> System.out.println(palamate[i]));
-            button.setTranslateX(50*i);//ボタンをずらす
-            layout.getChildren().add(button);//ボタン
-        });
+        //Layout 1 – children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1,button1);
+        scene1 = new Scene(layout1, 200, 200);
 
-        Scene scene = new Scene(layout,300,250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Button
+        Button button2 = new Button("This is scene sucks , go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //layout2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 900);
+
+        window.setScene(scene1);
+        window.setTitle("Title here");
+        window.show();
+
+
 
 
     }
