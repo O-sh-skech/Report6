dc132c3842d72617ff8a20fe1995a65006f7e13e
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2024-12-17 18:22:40 +0900

    調査4

--- app/src/main/java/jp/ac/uryukyu/ie/e245726/Sample.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245726/Sample.java
@@ -5,50 +5,32 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class Sample extends Application{
-
-    Stage window;
-    Scene scene1, scene2;
-
     public static void main(String[] args) {
         launch(args);
     }
 
-
+    Button button;
     
     @Override
     public void start(Stage primaryStage) throws Exception {
-        window = primaryStage;
+        primaryStage.setTitle("Title of the Window");
 
-        Label label1 = new Label("welcom to first scene");
-        Button button1 = new Button("go to scene2");
-        button1.setOnAction(e -> window.setScene(scene2));
-
-        //Layout 1 – children are laid out in vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1,button1);
-        scene1 = new Scene(layout1, 200, 200);
-
-        //Button
-        Button button2 = new Button("This is scene sucks , go back to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
-
-        //layout2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 900);
-
-        window.setScene(scene1);
-        window.setTitle("Title here");
-        window.show();
+        button = new Button("click me");//ボタン
+        button.setOnAction(e -> AlertBox.display("Title of window", "Wow this alert box is awsome"));
 
 
+        StackPane layout = new StackPane();//レイアウト
+        layout.getChildren().add(button);
+        
+
+        Scene scene = new Scene(layout,300,250);//シーン
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
 
     }
