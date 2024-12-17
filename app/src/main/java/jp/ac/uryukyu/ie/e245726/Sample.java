b787c1b90f93affaf2423cd36ceef708bc075e9e
Oshima Uta <e245726@ie.u-ryukyu.ac.jp>
2024-12-17 17:42:53 +0900

    project standed

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245726/Sample.java
@@ -0,0 +1,37 @@
+package jp.ac.uryukyu.ie.e245726;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Sample extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    Button button;
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+
+        button = new Button("click me");//ボタン
+        button.setOnAction(e -> System.out.println("hello"));
+
+
+        StackPane layout = new StackPane();//レイアウト
+        layout.getChildren().add(button);
+        
+
+        Scene scene = new Scene(layout,300,250);//シーン
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
